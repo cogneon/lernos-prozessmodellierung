3baecf22d08f7d9dd0cd0ce4dafb3710ac0fdef8
rednaxelaesor <loci@gmx.de>
2020-01-20 20:38:46 +0100

    Löschen build.sh

--- de/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-filename="lernOS-Prozessmodellierung-de"
-
-# install pandoc and pdflatex (best results with mactex) before
-
-echo Generating pdf for $filename
-pandoc $filename.md metadata.yaml -o $filename.pdf --from markdown --template ressources/Prozessmodellierung.latex --pdf-engine=pdflatex --number-sections -V lang=de-de
