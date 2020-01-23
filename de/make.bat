113aef2a2e0cb788166bad10d06617c5412af949
rednaxelaesor <loci@gmx.de>
2020-01-23 21:24:40 +0100

    Anpassungen für Windows
    / anstatt \ bei Pandoc
    ebook-Cover.png auch im Generate
    gitignore: Die Änderungen der Generate entfernt

--- de/make.bat
+++ de/make.bat
@@ -16,20 +16,19 @@ rmdir %dest_folder% /s /q
 mkdir %dest_folder%
 
 REM Create Microsoft Word Version (docx)
-pandoc -s -o %dest_folder%\%filename%.docx %filename%.md
+pandoc -s -o %dest_folder%/%filename%.docx %filename%.md
 
 REM Create Web Version (html)
-pandoc -s --toc -o %dest_folder%\%filename%.html %filename%.md
+pandoc -s --toc -o %dest_folder%/%filename%.html %filename%.md
 
 REM Create PDF Version (pdf)
-pandoc %filename%.md metadata.yaml -o %dest_folder%\%filename%.pdf --from markdown --template Prozessmodellierung.latex --number-sections -V lang=de-de
+pandoc %filename%.md metadata.yaml -o %dest_folder%/%filename%.pdf --from markdown --template Prozessmodellierung.latex --number-sections -V lang=de-de
 
 REM Create eBook Versions (epub, mobi)
-magick -density 300 %filename%.pdf[0] ebook-cover.png
-magick mogrify -size 2500x2500 -resize 2500x2500 images\ebook-cover.jpg
+magick -density 300 %dest_folder%/%filename%.pdf[0] %dest_folder%/ebook-cover.png
+magick mogrify -size 2500x2500 -resize 2500x2500 Images\ebook-cover.jpg
 magick mogrify -crop 1563x2500+102+0 images\ebook-cover.jpg
-pandoc metadata.yaml -s --epub-cover-image=ebook-cover.png -o %dest_folder%\%filename%.epub %filename%.md
-ebook-convert %dest_folder%\%filename%.epub %dest_folder%\%filename%.mobi
-del ebook-cover.jpg
-del ebook-cover.png
+pandoc metadata.yaml -s --epub-cover-image=%dest_folder%/ebook-cover.png -o %dest_folder%/%filename%.epub %filename%.md
+ebook-convert %dest_folder%/%filename%.epub %dest_folder%/%filename%.mobi
+del %dest_folder%\ebook-cover.png
 pause
